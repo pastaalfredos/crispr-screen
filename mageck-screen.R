139d2ee2e4aca01866b7a49a3cb07bb1c789853c
Alfred Lindqvist <alfred.lindqvist@hotmail.se>
2025-05-09 11:17:59 +0200

    Update mageck-screen.R for filtering quality sgRNAs

--- mageck-screen.R
+++ mageck-screen.R
@@ -4,3 +4,31 @@ library(dplyr)
 
 # Set file path to dataset
 file_path <- "data/13059_2020_1940_MOESM3_ESM.xlsx"
+
+# Read the dropout and cisplatin sheets
+dropout_data <- read_excel(file_path, sheet = "Dropout")
+cisplatin_data <- read_excel(file_path, sheet = "Cisplatin")
+
+# Check column names
+# colnames(dropout_data)
+colnames(cisplatin_data)
+
+# Sort for essential sgRNAs by low LFC and FDR
+essential_sgrnas <- dropout_data %>% 
+  select(sgrna = 1, gene = 2, lfc = 5, fdr = 12) %>% 
+  filter(lfc < -1, fdr < 0.05) %>% arrange(fdr)
+
+# Sort for drug resistant sgRNAs and senzitation sgRNAs
+res_sgrnas <- cisplatin_data %>%
+  select(sgrna = 1, gene = 2, lfc = 5, fdr = 12, high_in_treatment = 13) %>%
+  filter(lfc > 0, fdr < 0.05, high_in_treatment == TRUE) %>%
+  arrange(fdr)
+sens_sgrnas <- cisplatin_data %>%
+  select(sgrna = 1, gene = 2, lfc = 5, fdr = 12) %>%
+  filter(lfc < -1, fdr < 0.05) %>%
+  arrange(fdr)
+
+# View the output
+print(head(essential_sgrnas))
+print(head(res_sgrnas))
+print(head(sens_sgrnas))
