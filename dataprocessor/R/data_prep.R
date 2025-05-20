6e05199975cb0a56709b68a155b633c049db0d7c
Alfred Lindqvist <alfred.lindqvist@hotmail.se>
2025-05-20 12:54:58 +0200

    Convert sequences into one-hot format

--- dataprocessor/R/data_prep.R
+++ dataprocessor/R/data_prep.R
@@ -35,8 +35,31 @@ full_data <- na.omit(full_data)
 # Save the data into the project
 save(full_data, file = "data/full_data.rda")
 
+# One-hot encoding of sgRNA sequences
+one_hot_encode_seq <- function(seq) {
+  if (nchar(seq) != 20) stop("Sequence must be 20 nucleotides")
+  bases <- c("A", "C", "G", "T")
+  split_seq <- strsplit(seq, "")[[1]]
+  mat <- sapply(bases, function(base) as.integer(split_seq == base))
+  as.vector(t(mat))
+}
+
+# Apply one-hot encoding
+one_hot_matrix <- full_data$seq %>%
+  map(one_hot_encode_seq) %>%
+  do.call(rbind, .)
+
+# Convert matrix to a data frame and name the columns
+colnames(one_hot_matrix) <- paste0("base_", seq_len(ncol(one_hot_matrix)))
+one_hot_df <- as.data.frame(one_hot_matrix)
+
 # Simplify dataset to only have variables used for the predictive model
 ml_data <- full_data %>%
-  dplyr::select(sgrna, Gene, seq, gc_content, LFC, log_fpkm, start_position,
+  dplyr::select(sgrna, Gene, gc_content, LFC, log_fpkm, start_position,
          end_position, strand, transcript_biotype, gene_biotype)
+
+# Add sequence data
+ml_data <- bind_cols(ml_data, one_hot_df)
+
+# Save data
 save(ml_data, file = "data/ml_data.rda")
